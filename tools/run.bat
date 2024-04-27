559a7c744c3ebf37aa012c8a3c335c20f5f07638
ddukbaek2 <dagraac@naver.com>
2024-04-27 11:06:08 +0900

    work.

--- tools/run.bat
+++ tools/run.bat
@@ -2,15 +2,20 @@
 
 set BLENDERPATH=C:\Program Files\Blender Foundation\Blender 4.0
 set PYTHONPATH=C:\Program Files\Blender Foundation\Blender 4.0\4.0\python\bin
-set WORKSPACEPATH=%1
-set TOOLSPATH=%WORKSPACEPATH%\tools
+set PROJECTPATH=%1
+set TOOLSPATH=%PROJECTPATH%\tools
+set BUILDPATH=%PROJECTPATH%\build
+set SPECPATH=%PROJECTPATH%\spec
+for /f "tokens=1 delims=:" %%a in ("%PYTHONPATH%") do set "PYTHONDRIVE=%%a:"
+for /f "tokens=1 delims=:" %%a in ("%BLENDERPATH%") do set "BLENDERDRIVE=%%a:"
+for /f "tokens=1 delims=:" %%a in ("%PROJECTPATH%") do set "PROJECTDRIVE=%%a:"
 
-C:
+%PYTHONDRIVE%
 cd %PYTHONPATH%
 python.exe -m ensurepip --upgrade >nul 2>&1
 python.exe -m pip install --upgrade pip >nul 2>&1
 python.exe -m pip install --user -r %TOOLSPATH%\requirements.txt >nul 2>&1
 
-C:
+%BLENDERDRIVE%
 cd %BLENDERPATH%
-blender.exe --background --python %2 -- %3
+blender.exe --background --python %2 -- %3 %4
