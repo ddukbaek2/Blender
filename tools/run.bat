d46df4ab8dd24478d9724b916616744e92fdb97f
ddukbaek2 <dagraac@naver.com>
2024-04-27 10:54:14 +0900

    work.

--- tools/run.bat
+++ tools/run.bat
@@ -1,3 +1,5 @@
+@echo off
+
 set BLENDERPATH=C:\Program Files\Blender Foundation\Blender 4.0
 set PYTHONPATH=C:\Program Files\Blender Foundation\Blender 4.0\4.0\python\bin
 set WORKSPACEPATH=%1
@@ -5,7 +7,6 @@ set TOOLSPATH=%WORKSPACEPATH%\tools
 
 C:
 cd %PYTHONPATH%
-
 python.exe -m ensurepip --upgrade >nul 2>&1
 python.exe -m pip install --upgrade pip >nul 2>&1
 python.exe -m pip install --user -r %TOOLSPATH%\requirements.txt >nul 2>&1
