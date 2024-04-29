bd1bdac6f958e0f92e0bef14a2cf6088668f4bb8
ddukbaek2 <dagraac@naver.com>
2024-04-30 08:21:48 +0900

    work.

--- tools/build.bat
+++ tools/build.bat
@@ -3,7 +3,8 @@
 set BLENDERPATH=C:\Program Files\Blender Foundation\Blender 4.0
 set PYTHONPATH=%BLENDERPATH%\4.0\python\bin
 set PROJECTPATH=%1
-set TOOLSPATH=%PROJECTPATH%\tools	
+set TOOLSPATH=%PROJECTPATH%\tools
+set LIBSPATH=%PROJECTPATH%\libs
 set BUILDPATH=%PROJECTPATH%\build\bin
 set SPECPATH=%PROJECTPATH%\build\spec
 set HOOKSPATH=%PROJECTPATH%\hooks
@@ -19,4 +20,5 @@ python.exe -m pip list
 python.exe -m ensurepip --upgrade >nul 2>&1
 python.exe -m pip install --upgrade pip >nul 2>&1
 python.exe -m pip install --user -r "%TOOLSPATH%\requirements.txt" >nul 2>&1
+python.exe -m pip install --user -r "%LIBSPATH%\bpy-4.0.0-cp310-cp310-win_amd64.whl" >nul 2>&1
 python.exe -m PyInstaller -F --clean --onefile "%2" --distpath "%BUILDPATH%" --specpath "%SPECPATH%" --additional-hooks-dir="%HOOKSPATH%" --name "%3"
--- tools/run.bat
+++ tools/run.bat
@@ -4,6 +4,7 @@ set BLENDERPATH=C:\Program Files\Blender Foundation\Blender 4.0
 set PYTHONPATH=%BLENDERPATH%\4.0\python\bin
 set PROJECTPATH=%1
 set TOOLSPATH=%PROJECTPATH%\tools
+set LIBSPATH=%PROJECTPATH%\libs
 set BUILDPATH=%PROJECTPATH%\build\bin
 set SPECPATH=%PROJECTPATH%\build\spec
 set HOOKSPATH=%PROJECTPATH%\hooks
@@ -17,7 +18,8 @@ cd "%PYTHONPATH%"
 python.exe --version
 python.exe -m ensurepip --upgrade >nul 2>&1
 python.exe -m pip install --upgrade pip >nul 2>&1
-python.exe -m pip install --user -r %TOOLSPATH%\requirements.txt >nul 2>&1
+python.exe -m pip install --user -r "%TOOLSPATH%\requirements.txt" >nul 2>&1
+rem python.exe -m pip install --user -r "%LIBSPATH%\bpy-4.0.0-cp310-cp310-win_amd64.whl" >nul 2>&1
 
 %BLENDERDRIVE%
 cd %BLENDERPATH%
